862e73e3a2eb3f9a0c6aebdaf225bae05f83a963
robertatli <robbster10@gmail.com>
2017-03-14 14:29:46 +0000

    Add files via upload

---
+++ Liður 1 - verk 9/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>lidur 1 verkefni 9</title>
+	<link rel="stylesheet" type="text/css" href="stilsida.css">
+</head>
+	<body>
+	Scrolled <span id="scroll">0</span> times.<br>
+	<script src="lid1v9.js"></script>
+	</body>
+</html>
---
+++ Liður 1 - verk 9/lid1v9.js
@@ -0,0 +1,4 @@
+var x = 0;
+window.addEventListener('scroll', function(e){
+    document.getElementById("scroll").innerHTML = x += 1;
+});
